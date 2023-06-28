26fbe49a49c173cf1a9d8a14c949481fe2168a71
coderella <qiangie.yang@gmail.com>
2023-06-28 16:51:15 +0100

    Add test cases and mocks to check the credit function

---
+++ spec/account.spec.js
@@ -0,0 +1,47 @@
+import Account from "../src/Account.js";
+
+describe('Account Test', () => {
+
+    let mockBalance;
+    let mockTransaction;
+    let testAccount;
+
+    beforeEach(() => {
+        mockTransaction = {
+            getDate: () => { },
+            getDepositAmount: () => { },
+            getWithdrawAmount: () => { }
+        };
+
+        mockBalance = {
+            getBalance: () => { },
+            add: () => { },
+            withdraw: () => { }
+        }
+
+        testAccount = new Account(mockBalance);
+    });
+
+    describe('Credit function', () => {
+
+        it('should call Transaction getDepositAmount when crediting the account with the transaction data', () => {
+            // Arrange
+            const mockTransactionSpy = spyOn(mockTransaction, `getDepositAmount`)
+            // Act
+            testAccount.credit(mockTransaction);
+            // Assert
+            expect(mockTransactionSpy).toHaveBeenCalled();
+        });
+
+        it('should call Transaction getDate when crediting the account', () => {
+            // Arrange
+            const mockTransactionSpy = spyOn(mockTransaction, `getDate`)
+            // Act
+            testAccount.credit(mockTransaction);
+            // Assert
+            expect(mockTransactionSpy).toHaveBeenCalled();
+        });
+
+    });
+
+});
--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -4,43 +4,40 @@ describe('Transaction', () => {
 
     describe('Constructor', () => {
         it('should create a transaction object with the provided date, deposit amount, and withdrawal amount', () => {
-            const date = new Date(); // cannot be 14/01/2012, because in js the Date constructor expects the date string to be in ISO 8601 format (e.g., 'YYYY-MM-DD').
+            // Arrange
+            const date = '2012-01-14'; // cannot be 14/01/2012, because in js the Date constructor expects the date string to be in ISO 8601 format (e.g., 'YYYY-MM-DD').
             const depositAmount = 1000;
             const withdrawalAmount = 500;
 
             const transaction = new Transaction(date, depositAmount, withdrawalAmount);
-
+            // Act
+            // Assert
             expect(transaction.getDate()).toEqual(date);
             expect(transaction.getDepositAmount()).toEqual(depositAmount);
             expect(transaction.getWithdrawAmount()).toEqual(withdrawalAmount);
         });
-    })
+    });
 
     describe('Transaction date input', () => {
         it('should throw an error if date is not provided', () => {
-
             const transaction = new Transaction('', 1000, '');
 
             expect(() => { transaction.getDate() }).toThrow(new Error("Must provide a valid date!"));
         });
 
         it('should throw an error if date is not a valid date', () => {
-
-            const dateTest = new Date('rguiwtyhg');
+            const dateTest = 'Not a date';
             const transaction = new Transaction(dateTest, 1000, '');
 
             expect(() => { transaction.getDate() }).toThrow(new Error("Must provide a valid date!"));
         });
 
         it('should throw an error if date is not YYYY-MM-DD type', () => { //decided to force date format into standard instead to split day month year and check if isNAN
-
             const dateTest = '14/01/2012';
             const transaction = new Transaction(dateTest, 1000, '');
 
             //console.log(Date.parse(dateTest)); return NaN
             expect(() => { transaction.getDate() }).toThrow(new Error("Must provide a valid date!"));
-        })
-    })
-
-
+        });
+    });
 });
