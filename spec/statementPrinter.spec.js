2933c1e8d01526e29642d2b7d16bea846d98f6fe
coderella <qiangie.yang@gmail.com>
2023-06-28 20:28:54 +0100

    Add date format test

--- spec/statementPrinter.spec.js
+++ spec/statementPrinter.spec.js
@@ -1,3 +1,5 @@
+import StatementPrinter from "../src/StatementPrinter.js"
+
 describe('Statement printer test', () => {
 
     let statementPrinter;
@@ -7,8 +9,12 @@ describe('Statement printer test', () => {
     });
 
     it('should print the header row as expected', () => {
-
-        const result = StatementPrinter.printerHeader();
+        const result = StatementPrinter.printHeader();
         expect(result).toBe('date       || credit  || debit  || balance');
     });
+
+    it('should print date in DD/MM/YYYY', () => {
+        const result = StatementPrinter.formatDate('2012-01-10');
+        expect(result).toBe('10/01/2012');
+    })
 });
