b1994c8a582ae737fcba9e81e0a55adc3da74463
coderella <qiangie.yang@gmail.com>
2023-06-28 20:29:41 +0100

    Implement formatDate function

--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -1,12 +1,15 @@
 class StatementPrinter {
 
 
-    static printerHeader() {
+    static printHeader() {
         const headerRow = 'date       || credit  || debit  || balance';
-        console.log(headerRow);
         return headerRow;
     };
 
+    static formatDate(dateString) {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    };
 
 }
 
