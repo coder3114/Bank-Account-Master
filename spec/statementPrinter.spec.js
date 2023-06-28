c5262105209ca6745f547bd7f8ce798d907aad1b
coderella <qiangie.yang@gmail.com>
2023-06-28 22:12:05 +0100

    Add trim() to amounts, refactor code

--- spec/statementPrinter.spec.js
+++ spec/statementPrinter.spec.js
@@ -22,17 +22,17 @@ describe('Statement printer test', () => {
 
         it('should print credit amount in 2 decimal points', () => {
             const result = StatementPrinter.formatCredit(1000);
-            expect(result).toBe('1000.00');
+            expect(result.trim()).toBe('1000.00');
         });
 
         it('should print debit amount in 2 decimal points', () => {
             const result = StatementPrinter.formatCredit(500);
-            expect(result).toBe('500.00');
+            expect(result.trim()).toBe('500.00');
         });
 
         it('should print balance amount in 2 decimal points', () => {
             const result = StatementPrinter.formatBalance(2500);
-            expect(result).toBe('2500.00');
+            expect(result.trim()).toBe('2500.00');
         });
     });
 
@@ -42,20 +42,5 @@ describe('Statement printer test', () => {
             const result = StatementPrinter.printRow(transactionData);
             expect(result).toBe('10/01/2012 || 2000.00 ||        || 3000.00')
         });
-    });
-
-    describe('Console logging', () => {
-        let clgSpy;
-        const txnHistory = [['2012-01-10', 2000, null, 3000], ['2012-01-10', 2000, null, 3000], ['2012-01-10', 2000, null, 3000]];
-
-        beforeEach(() => {
-            clgSpy = spyOn(console, "log");
-        });
-
-        it('should call console.log the number of times the length of the array of transaction history is', () => {
-            StatementPrinter.print(txnHistory);
-            expect(clgSpy).toHaveBeenCalledTimes(txnHistory.length)
-        });
-
     });
 });
