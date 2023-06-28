9b95c0c7cb20a9b68b864fea0275c14dcfb24a6b
coderella <qiangie.yang@gmail.com>
2023-06-28 20:38:11 +0100

    Implement formatCredit function

--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -11,6 +11,11 @@ class StatementPrinter {
         return date.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
     };
 
+    static formatCredit(credit) {
+        const roundedCredit = credit != null ? parseFloat(credit).toFixed(2) : '';
+        return roundedCredit;
+    };
+
 }
 
 export default StatementPrinter;
