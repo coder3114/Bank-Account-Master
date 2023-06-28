71b0801bbe50ca6194ea337ff326badb85409bbb
coderella <qiangie.yang@gmail.com>
2023-06-28 17:40:34 +0100

    Implement Account debit function

--- src/Account.js
+++ src/Account.js
@@ -14,13 +14,21 @@ class Account {
         this.#txnHistory.push(tempTransaction);
     };
 
+    debit(transaction) {
+        const debit = transaction.getWithdrawAmount();
+        this.#accountBalance.withdraw(debit);
+        const tempTransaction = [transaction.getDate(), '', debit, this.#accountBalance.getBalance()];
+        this.#txnHistory.push(tempTransaction);
+    };
+
     getBalance() {
         return this.#accountBalance.getBalance();
     };
 
     getTransactions() {
         return this.#txnHistory;
-    };
+    }
+
 };
 
 export default Account;
